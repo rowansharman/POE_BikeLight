d2570ff6d9d822675a6ac0d33acb2a1ac92975ea
Rowan Sharman <rsharman@olin.edu>
2017-09-01 10:24:28 -0400

    define pins and pinmodes

--- BikeLight/BikeLight.ino
+++ BikeLight/BikeLight.ino
@@ -1,6 +1,16 @@
-void setup() {
-  // put your setup code here, to run once:
+const byte led1 = 12;
+const byte led2 = 11;
+const byte led3 = 10;
+const byte led4 = 9;
+const byte button = 3;
+const byte pot = 4;
 
+void setup() {
+  pinMode(12, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(3, INPUT);
 }
 
 void loop() {
