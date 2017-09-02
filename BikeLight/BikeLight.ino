c292c8ae0d6fde738035da58b9942c25e4dd4316
Emma Westerhoff <emma.westerhoff@students.olin.edu>
2017-09-02 18:52:28 -0400

    part 2 of lab, pot ajusts brightness in 5th case

--- BikeLight/BikeLight.ino
+++ BikeLight/BikeLight.ino
@@ -1,12 +1,13 @@
-const byte led1 = 12;
+const byte led1 = 6;
 const byte led2 = 11;
 const byte led3 = 10;
 const byte led4 = 9;
 const byte button = 3;
-const byte pot = 4;
+const byte potPin = 0;
 int value = 0;
 int value1 = 0;
 int counter = 0;
+int potState = 0;
 
 void setup() {
   Serial.begin(9600);
@@ -27,7 +28,7 @@ void loop() {
 
   value = value1;
     
-  counter = counter%5;
+  counter = counter%6;
   
   switch (counter) {
     case 1:
@@ -74,6 +75,14 @@ void loop() {
       digitalWrite(led4, HIGH);
       delay(200);
       break; 
+    case 5:
+      potState = analogRead(potPin);
+      potState = map(potState, 0, 1023, 0, 255); //maps pot input of 0 to 1023 to led output of 0 to 255
+      analogWrite(led1, potState);
+      analogWrite(led2, potState);
+      analogWrite(led3, potState);
+      analogWrite(led4, potState);
+      break;
     default:
       digitalWrite(led1, LOW);
       digitalWrite(led2, LOW);
